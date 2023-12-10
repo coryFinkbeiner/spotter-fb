78371b47a04ae6e80fe8993f76d795904ce060d4
steeper <portland.steeper@gmail.com>
2023-12-10 18:21:25 -0500

    added helloWorld fb function, about to test

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+
+
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+
+
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -19,3 +23,12 @@ const logger = require("firebase-functions/logger");
 // });
 
 
+
+
+
+
+import * as functions from 'firebase-functions';
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('HELLO')
+})
