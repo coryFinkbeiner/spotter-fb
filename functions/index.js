c1f66fad7c2941525faab4f06056ff8e67582889
steeper <portland.steeper@gmail.com>
2023-12-10 23:59:44 -0500

    login and refresh firebase functions showing up in console

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,16 @@
 const functions = require("firebase-functions");
 const SpotifyWebApi = require('spotify-web-api-node');
 
+const clientId = functions.config().spotify.clientid;
+const clientSecret = functions.config().spotify.clientsecret;
+const redirectUri = functions.config().spotify.redirecturi;
+
+const spotifyApi = new SpotifyWebApi({
+  redirectUri: redirectUri,
+  clientId: clientId,
+  clientSecret: clientSecret,
+});
+
 // Test function
 exports.helloWorld = functions.https.onRequest((req, res) => {
   res.json({ message: "Hello, World!" });
@@ -10,13 +20,6 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
 exports.refreshToken = functions.https.onRequest(async (req, res) => {
   try {
     const refreshToken = req.body.refreshToken;
-    const spotifyApi = new SpotifyWebApi({
-      redirectUri: process.env.REDIRECT_URI,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken,
-    });
-
     const data = await spotifyApi.refreshAccessToken();
     res.json({
       accessToken: data.body.accessToken,
@@ -31,12 +34,6 @@ exports.refreshToken = functions.https.onRequest(async (req, res) => {
 exports.login = functions.https.onRequest(async (req, res) => {
   try {
     const code = req.body.code;
-    const spotifyApi = new SpotifyWebApi({
-      redirectUri: process.env.REDIRECT_URI,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-    });
-
     const data = await spotifyApi.authorizationCodeGrant(code);
     res.json({
       accessToken: data.body.access_token,
