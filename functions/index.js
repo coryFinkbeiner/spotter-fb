3f7eca3e64b618d416404f4a5db8dabb4e1d2866
steeper <portland.steeper@gmail.com>
2023-12-10 21:39:26 -0500

    going to trty to init hosting with Express

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,9 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
-
-
-
-
-// import * as functions from 'firebase-functions';
-
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   response.send('HELLO')
-// })
-
-
-
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-exports.yourFunctionName = functions.https.onRequest((request, response) => {
-  // Your function logic here
-  response.send('Hello, Firebase!');
-});
+// Import your Express app
+const app = require('./api');
+
+// Define Cloud Functions
+exports.api = functions.https.onRequest(app);
