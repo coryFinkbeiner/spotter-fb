1852e0aa937701d80148ab552b6cf6de20ce764b
steeper <portland.steeper@gmail.com>
2023-12-18 20:44:19 -0500

    test fetch works

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css'
-
 import useSpotifyAuth from './hooks/useSpotifyAuth';
-
 
 function App({code}) {
 
   const accessToken = useSpotifyAuth(code);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios({
+          method: 'GET',
+          url: `https://api.spotify.com/v1/me/albums`,
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          params: {
+            limit: 10,
+          },
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.log('API error', error);
+      }
+    })();
+  }, [accessToken]);
 
 
   return (
